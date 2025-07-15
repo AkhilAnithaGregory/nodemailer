b4a85baf432234f35f245c8fbeb4ca441a9eecc5
Akhil Gregory <bw974425@gmail.com>
2025-07-15 16:39:08 +0530

    Code updated

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -15,8 +14,8 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "email", // Replace with your Gmail address
+    pass: "passcode", // Check Readme for details
   },
 });
 
@@ -24,33 +23,10 @@ app.post("/contact", (req, res) => {
   const { email, name, subject, message } = req.body;
 
   const mailOptions = {
-    from: "Portfolio <akhilagiantz@gmail.com>",
-    to: "bw974425@gmail.com",
+    from: '"Label" <email>', // Replace with your from Gmail address
+    to: "email", // Replace with your to Gmail address
     subject: `${subject}`,
     text: `${name}\n${email}\n${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.json({
-        success: false,
-        message: "There was an error sending the email!",
-      });
-    } else {
-      res.json({ success: true, message: "Email sent successfully!" });
-      addLabelToEmail(info.messageId);
-    }
-  });
-});
-
-app.post("/enquiry/send", (req, res) => {
-  const { Name, Email, Number, Subject, Message } = req.body;
-
-  const mailOptions = {
-    from: "Portfolio <akhilagiantz@gmail.com>",
-    to: "kavyavijayan6713@gmail.com",
-    subject: `${Subject}`,
-    text: `${Name}\n${Email}\n${Number}\n${Message}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
